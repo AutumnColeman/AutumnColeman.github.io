85bca699ca16c8b62972003bb07d2ef4cd8bb4e7
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-12-08 21:41:52 -0500

    Added images

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
   .state({
     name: 'home',
     url: '/',
-    templateUrl: 'index.html'
+    templateUrl: 'main.html'
   })
   .state({
     name: 'about',
@@ -26,3 +26,33 @@ app.config(function($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/');
 });
+
+window.onload = function(){
+    // your code
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+};
